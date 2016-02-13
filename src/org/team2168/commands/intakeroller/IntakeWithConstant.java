2eef99216c88033d76125401038f0391f098d623
Benmw99 <benmw10@gmail.com>
2016-02-13 11:53:51 -0500

    removed all the old intake stuff

--- src/org/team2168/commands/intakeroller/IntakeWithConstant.java
+++ src/org/team2168/commands/intakeroller/IntakeWithConstant.java
@@ -24,7 +24,7 @@ public class IntakeWithConstant extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-		Robot.intake.driveIntake(speed);
+		Robot.intakeRoller.driveIntake(speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
