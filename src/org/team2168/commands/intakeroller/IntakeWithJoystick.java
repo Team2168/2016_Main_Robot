f57d4b7ed06e1804aea839bcd2e6413dfeedc015
notincontrol <kevin@team2168.org>
2016-02-13 14:39:23 -0500

    fixes incorrect static Joystick instances in OI

--- src/org/team2168/commands/intakeroller/IntakeWithJoystick.java
+++ src/org/team2168/commands/intakeroller/IntakeWithJoystick.java
@@ -23,7 +23,7 @@ public class IntakeWithJoystick extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.intakeRoller.driveIntake(OI.operatorJoystick.getLeftStickRaw_Y());
+    	Robot.intakeRoller.driveIntake(Robot.oi.operatorJoystick.getLeftStickRaw_Y());
     }
 
     // Make this return true when this Command no longer needs to run execute()
