b2a855474e1bd3e561c57025f90ca110b4183c55
James <james@team2168.org>
2016-02-19 23:44:08 -0500

    Don't need to comment these out. If they aren't called they won't run. We can still have them available if we want to use them in the future.

--- src/org/team2168/commands/indexer/DriveIndexerWithJoysticks.java
+++ src/org/team2168/commands/indexer/DriveIndexerWithJoysticks.java
@@ -1,40 +1,39 @@
-//package org.team2168.commands.indexer;
-//
-//import org.team2168.Robot;
-//import org.team2168.RobotMap;
-//
-//import edu.wpi.first.wpilibj.command.Command;
-//
-///**
-// * Drives the indexer using the right joystick on the operator controller
-// */
-//public class DriveIndexerWithJoysticks extends Command {
-//
-//    public DriveIndexerWithJoysticks() {
-//        // Use requires() here to declare subsystem dependencies
-//         requires(Robot.indexer);
-//    }
-//
-//    // Called just before this Command runs the first time
-//    protected void initialize() {
-//    }
-//
-//    // Called repeatedly when this Command is scheduled to run
-//    protected void execute() {
-//    	Robot.indexer.setSpeed(Robot.oi.operatorJoystick.getRightStickRaw_Y());
-//    }
-//
-//    // Make this return true when this Command no longer needs to run execute()
-//    protected boolean isFinished() {
-//        return false;
-//    }
-//
-//    // Called once after isFinished returns true
-//    protected void end() {
-//    }
-//
-//    // Called when another command which requires one or more of the same
-//    // subsystems is scheduled to run
-//    protected void interrupted() {
-//    }
-//}
+package org.team2168.commands.indexer;
+
+import org.team2168.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Drives the indexer using the right joystick on the operator controller
+ */
+public class DriveIndexerWithJoysticks extends Command {
+
+    public DriveIndexerWithJoysticks() {
+        // Use requires() here to declare subsystem dependencies
+         requires(Robot.indexer);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.indexer.setSpeed(Robot.oi.operatorJoystick.getRightStickRaw_Y());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
