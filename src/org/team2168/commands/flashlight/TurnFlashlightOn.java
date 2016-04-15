c1688c8cfea968976623421c2f6fc8bcbfe31b97
notincontrol <kevin@team2168.org>
2016-04-15 11:48:20 -0400

    working on flashlight

---
+++ src/org/team2168/commands/flashlight/TurnFlashlightOn.java
@@ -0,0 +1,44 @@
+package org.team2168.commands.flashlight;
+
+import org.team2168.Robot;
+import org.team2168.subsystems.Flashlight;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class TurnFlashlightOn extends Command {
+
+	Flashlight flashlight;
+	
+    public TurnFlashlightOn() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.flashlight);
+    	flashlight = Robot.flashlight;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	flashlight.setFlashlightOn();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
