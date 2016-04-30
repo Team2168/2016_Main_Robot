cf2e13de22cb85699e3f2e916c5cc636e75fc41b
James <james@team2168.org>
2016-04-30 05:08:18 -0400

    Added PC some auto modes. These need to be tested!

---
+++ src/org/team2168/commands/auto/DriveUnderPCAndFireCenterSecond.java
@@ -0,0 +1,50 @@
+package org.team2168.commands.auto;
+
+import org.team2168.RobotMap;
+import org.team2168.commands.drivetrain.PIDCommands.DrivePIDPause;
+import org.team2168.commands.drivetrain.PIDCommands.RotateXDistancePIDZZZ;
+import org.team2168.commands.drivetrain.PIDCommands.RotateXDistancePIDZZZCameraWithGyro;
+import org.team2168.commands.indexer.DriveIndexerWithConstant;
+import org.team2168.commands.intakeroller.IntakeWithConstant;
+import org.team2168.commands.shooter.PIDCommands.DriveShooterPIDSpeed;
+import org.team2168.commands.shooter.PIDCommands.ShooterPIDPause;
+import org.team2168.commands.shooter.PIDCommands.WaitForShooterPIDToFinish;
+import org.team2168.commands.shooterPneumatics.ShooterHoodFarShotPosition;
+import org.team2168.commands.shooterPneumatics.ShooterHoodStowPosition;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
+/**
+ * Drives Robot over defense
+ */
+public class DriveUnderPCAndFireCenterSecond extends CommandGroup {
+    
+    public  DriveUnderPCAndFireCenterSecond() {
+    	addSequential(new DriveUnderPCAuto());
+    	addSequential(new RotateXDistancePIDZZZ(180 + 30, 0.8, 0.25, 1));
+    	
+    	//THE REST IS THE SAME AS THE VANILLA CenterSecond AUTO
+    	
+    	//Put up hood and rotate
+    	addParallel(new ShooterHoodFarShotPosition());
+    	addParallel(new DriveShooterPIDSpeed(6500));
+    	
+    	addSequential(new Sleep(), 0.7); // camera lag
+    	addSequential(new RotateXDistancePIDZZZCameraWithGyro(0, RobotMap.ROTATE_POSITION_CAMERA_MAX, RobotMap.ROTATE_POSITION_CAMERA_MIN, 0.5));
+//    	addSequential(new Sleep(),2);
+    	
+    	addSequential(new WaitForShooterPIDToFinish());
+    	
+//    	//Fire for 3 seconds
+    	addParallel(new DriveIndexerWithConstant(RobotMap.INDEXER_SPEED_CONSTANT_SHOOT), 1);
+    	addParallel(new IntakeWithConstant(RobotMap.INTAKE_SPEED_CONSTANT_SHOOT), 1);
+    	addSequential(new Sleep(), 3);
+    	
+//    	//Clean Up Robot
+    	addParallel(new ShooterHoodStowPosition());
+    	addParallel(new ShooterPIDPause());
+    	addParallel(new DrivePIDPause());
+    	addParallel(new DriveIndexerWithConstant(0));
+    	addParallel(new IntakeWithConstant(0));
+    }
+}
---
+++ src/org/team2168/commands/auto/DriveUnderPCAuto.java
@@ -0,0 +1,42 @@
+package org.team2168.commands.auto;
+
+import org.team2168.RobotMap;
+import org.team2168.commands.drivetrain.DriveWithConstant;
+import org.team2168.commands.drivetrain.PIDCommands.DrivePIDPause;
+import org.team2168.commands.drivetrain.PIDCommands.DriveXDistance;
+import org.team2168.commands.drivetrain.PIDCommands.RotateXDistancePIDZZZ;
+import org.team2168.commands.drivetrain.PIDCommands.RotateXDistancePIDZZZCamera;
+import org.team2168.commands.drivetrain.PIDCommands.RotateXDistancePIDZZZCameraWithGyro;
+import org.team2168.commands.indexer.DriveIndexerWithConstant;
+import org.team2168.commands.intakeposition.IntakeExtend;
+import org.team2168.commands.intakeroller.IntakeWithConstant;
+import org.team2168.commands.portCullus.PCManipulatorExtend;
+import org.team2168.commands.shooter.PIDCommands.DriveShooterPIDSpeed;
+import org.team2168.commands.shooter.PIDCommands.ShooterPIDPause;
+import org.team2168.commands.shooter.PIDCommands.WaitForShooterPIDToFinish;
+import org.team2168.commands.shooterPneumatics.ShooterHoodCloseShotPosition;
+import org.team2168.commands.shooterPneumatics.ShooterHoodFarShotPosition;
+import org.team2168.commands.shooterPneumatics.ShooterHoodStowPosition;
+import org.team2168.commands.shooterhood.DriveShooterHoodToAngle;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
+/**
+ * Drives Robot under the Portcullis
+ * 
+ * In this auto mode, the robot starts with the intake facing away from the defenses.
+ * The PC manipulator is starting facing the PC
+ */
+public class DriveUnderPCAuto extends CommandGroup {
+    
+    public  DriveUnderPCAuto() {
+    	//Lower everything
+    	addParallel(new PCManipulatorExtend());
+    	addParallel(new ShooterHoodStowPosition());
+    	addSequential(new IntakeExtend());  //no timeout, if it  doesn't lower, don't proceed
+    	
+    	//Drive under the PC
+    	addSequential(new DriveXDistance(17.5, 0.5, 0.5));
+    	addSequential(new DriveXDistance(-1.5, 0.5, 0.5));
+    }
+}
