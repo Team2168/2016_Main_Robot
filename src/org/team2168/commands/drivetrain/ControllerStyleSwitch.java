c4733316ddb7a8d1e7a9a4578102e243e84ead05
ImSovietBear <elijah.r223@gmail.com>
2016-11-06 12:14:59 -0500

    added basic functions of the ControllerStyleSwitch command

--- src/org/team2168/commands/drivetrain/ControllerStyleSwitch.java
+++ src/org/team2168/commands/drivetrain/ControllerStyleSwitch.java
@@ -1,17 +1,43 @@
 package org.team2168.commands.drivetrain;
 
+import org.team2168.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
+
+
+/**Choose and use a certain control style.
+ *@author Ben Waid, Elijah Reeds, Peter Dentch
  */
 public class ControllerStyleSwitch extends Command {
-
+	
+	private static int crtlStyle = 0;
+	
+	
     public ControllerStyleSwitch() {
         // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(Robot.drivetrain);
+    }
+    
+    /**Sets the current control style.
+     * @author Ben Waid, Elijah Reeds, Peter Dentch
+	 * @param Control style.
+	 */
+    public void setCrtlStyle(int style){
+    	//Edit this as we make control styles with a case/if else 
+    	//structure to make sure no invalid numbers are put in.
+    	
+    	crtlStyle = style;
     }
 
+    /**Gets the current Control Style.
+     * @author Elijah Reeds
+     * @return Current Control Style.
+     */
+    public int getCrtlStyle(){
+    	return(crtlStyle);
+    }
+    
     // Called just before this Command runs the first time
     protected void initialize() {
     }
