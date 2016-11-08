1437f9a6442ef5c6d34cd549e3302bdef31c6549
Benmw99 <benmw10@gmail.com>
2016-11-07 20:02:55 -0500

    Added into ControllerSwitchStyle different driving methods. NEEDS TO BE TESTED

--- src/org/team2168/commands/drivetrain/ControllerStyleSwitch.java
+++ src/org/team2168/commands/drivetrain/ControllerStyleSwitch.java
@@ -11,39 +11,68 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class ControllerStyleSwitch extends Command {
 	
-	private static int crtlStyle = 0;
+	int ctrlStyle;
 	
-	
-    public ControllerStyleSwitch() {
-        // Use requires() here to declare subsystem dependencies
-        requires(Robot.drivetrain);
-    }
-    
-    /**Sets the current control style.
-     * @author Ben Waid, Elijah Reeds, Peter Dentch
-	 * @param Control style.
+	/**
+	 * Controller Styles
+	 * 0 = Tank Drive
+	 * 1 = Arcade Drive
+	 * 2 = GTA
+	 * @param inputStyle
 	 */
-    public void setCrtlStyle(int style){
-    	//Edit this as we make control styles with a case/if else 
-    	//structure to make sure no invalid numbers are put in.
-    	
-    	crtlStyle = style;
+    public ControllerStyleSwitch(int inputStyle) {
+        requires(Robot.drivetrain);
+        ctrlStyle = inputStyle;
     }
 
-    /**Gets the current Control Style.
-     * @author Elijah Reeds
-     * @return Current Control Style.
-     */
-    public int getCrtlStyle(){
-    	return(crtlStyle);
-    }
-    
     // Called just before this Command runs the first time
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
+    //TODO NEEDS TESTING ON ACTUAL ROBOT
     protected void execute() {
+    	/**
+    	 *Tank Drive
+    	 */
+    	if(ctrlStyle == 0){
+        	Robot.drivetrain.driveLeft(Robot.oi.driverJoystick.getLeftStickRaw_Y());
+        	Robot.drivetrain.driveRight(Robot.oi.driverJoystick.getRightStickRaw_Y());
+    	}
+    	
+    	/**
+    	 * Arcade Drive
+    	 */
+    	else if(ctrlStyle == 1) {
+    		Robot.drivetrain.driveLeft(Robot.oi.driverJoystick.getLeftStickRaw_Y() + Robot.oi.driverJoystick.getRightStickRaw_X());
+    		Robot.drivetrain.driveRight(Robot.oi.driverJoystick.getLeftStickRaw_Y() - Robot.oi.driverJoystick.getRightStickRaw_X());
+    	}
+    	
+    	/**
+    	 * GTA Drive
+    	 */
+    	else if(ctrlStyle == 2) {
+    		double fwdSpeed = Robot.oi.driverJoystick.getRightTriggerAxisRaw();
+    		double revSpeed = Robot.oi.driverJoystick.getLeftTriggerAxisRaw();
+    		double speed = fwdSpeed - revSpeed;
+    		
+    		//Allows Robot to spin in place without needing to press in triggers
+    		if(speed != 0){
+    			Robot.drivetrain.driveLeft(Robot.oi.driverJoystick.getRightStickRaw_X() * speed);
+    			Robot.drivetrain.driveRight(-(Robot.oi.driverJoystick.getRightStickRaw_X()) * speed);
+    		}
+    		else if(speed == 0) {
+    			Robot.drivetrain.driveLeft(Robot.oi.driverJoystick.getRightStickRaw_X());
+    			Robot.drivetrain.driveRight(-(Robot.oi.driverJoystick.getRightStickRaw_X()));
+    		}
+    	}
+    	
+    	/**
+    	 * Default back to tank drive if input is invalid
+    	 */
+    	else {
+    		ctrlStyle = 0;
+    	}
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
